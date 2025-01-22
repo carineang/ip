8acd75a62754b57ab47ce539407b4eb842129289
carineang <carineang@users.noreply.github.com>
2025-01-23 04:17:25 +0800

    Add store and list user input

--- src/main/java/TaskBuddy.java
+++ src/main/java/TaskBuddy.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 
 public class TaskBuddy {
     public static void main(String[] args) {
+        // Initialize storage for maximum 100 tasks
+        String[] tasks = new String[100];
+        // Keep track of the number of tasks
+        int taskCount = 0;
+
         System.out.println("Hello! I'm TaskBuddy");
         System.out.println("What can I do for you?");
 
-        // detect user input
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(userInput);
-            userInput = sc.nextLine();
+        while (true) {
+            String userInput = sc.nextLine();
+            // List all tasks
+            if (userInput.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+            // bye to exit
+            } else if (userInput.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                break;
+            // add new task to task list
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("    added: " + userInput);
+            }
         }
-
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
