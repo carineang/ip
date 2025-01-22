711fb222a344f703d493b2a4cce6a9cedb6db90c
carineang <carineang@users.noreply.github.com>
2025-01-23 03:31:57 +0800

    Add chatbot rename, greet, exit messages

---
+++ src/main/java/TaskBuddy.java
@@ -0,0 +1,8 @@
+public class TaskBuddy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm TaskBuddy");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
