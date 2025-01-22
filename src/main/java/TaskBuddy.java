0192371ab8a415752888cf7cd9979247ae6aab93
carineang <carineang@users.noreply.github.com>
2025-01-23 03:47:21 +0800

    Add echo

--- src/main/java/TaskBuddy.java
+++ src/main/java/TaskBuddy.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class TaskBuddy {
     public static void main(String[] args) {
         System.out.println("Hello! I'm TaskBuddy");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
 
+        // detect user input
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
