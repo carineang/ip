68b78f107c96f421296a5e8120084c09700200b4
carineang <carineang@users.noreply.github.com>
2025-01-23 15:40:16 +0800

    Add mark as done

---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    // If task is completed
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    // If task is not completed
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
--- src/main/java/TaskBuddy.java
+++ src/main/java/TaskBuddy.java
@@ -1,32 +1,63 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskBuddy {
     public static void main(String[] args) {
-        // Initialize storage for maximum 100 tasks
-        String[] tasks = new String[100];
-        // Keep track of the number of tasks
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
+        String indent = "   ";
 
         System.out.println("Hello! I'm TaskBuddy");
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             String userInput = sc.nextLine();
+            // Split the input into 2 parts
+            String[] inputParts = userInput.split(" ", 2);
+            String command = inputParts[0];
+
             // List all tasks
-            if (userInput.equals("list")) {
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+            if (command.equals("list")) {
+                System.out.println(indent + "Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(indent + (i + 1) + "." + tasks.get(i));
                 }
-            // bye to exit
-            } else if (userInput.equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!");
+
+            // Mark this task as completed
+            } else if (command.equals("mark")) {
+                try {
+                    // Convert String to Int
+                    int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+                    tasks.get(taskNumber).markAsDone();
+                    System.out.println(indent + "Nice! I've marked this task as done:");
+                    System.out.println(indent + "  " + tasks.get(taskNumber));
+                } catch (Exception e) {
+                    System.out.println(indent + "Invalid task number.");
+                }
+
+            // Mark this task as not completed
+            } else if (command.equals("unmark")) {
+                try {
+                    // Convert String to Int
+                    int taskNumber = Integer.parseInt(inputParts[1]) - 1;
+                    tasks.get(taskNumber).markAsNotDone();
+                    System.out.println(indent + "OK, I've marked this task as not done yet:");
+                    System.out.println(indent + "  " + tasks.get(taskNumber));
+                } catch (Exception e) {
+                    System.out.println(indent + "Invalid task number.");
+                }
+
+            // Bye to exit
+            } else if (command.equals("bye")) {
+                System.out.println(indent + "Bye. Hope to see you again soon!");
                 break;
-            // add new task to task list
+
+            // Add new task to new task list
             } else {
-                tasks[taskCount] = userInput;
-                taskCount++;
-                System.out.println("    added: " + userInput);
+                Task newTask = new Task(userInput);
+                tasks.add(newTask);
+                System.out.println(indent + "added: " + userInput);
             }
         }
     }
