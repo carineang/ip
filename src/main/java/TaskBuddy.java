266175b161f840e36493bfa3c60ebdfb92ff120c
carineang <carineang@users.noreply.github.com>
2025-01-23 17:10:28 +0800

    Add todos, events, deadlines

--- src/main/java/TaskBuddy.java
+++ src/main/java/TaskBuddy.java
@@ -48,17 +48,47 @@ public class TaskBuddy {
                     System.out.println(indent + "Invalid task number.");
                 }
 
+            // To-Do tasks
+            } else if (command.equals("todo")) {
+                String description = inputParts[1];
+                Task newTask = new Todo(description);
+                tasks.add(newTask);
+                addTaskMessage(newTask, tasks.size());
+
+            // Deadline tasks
+            } else if (command.equals("deadline")) {
+                String[] deadlineParts = inputParts[1].split(" /by ", 2);
+                String description = deadlineParts[0];
+                String deadline = deadlineParts[1];
+                Task newTask = new Deadline(description, deadline);
+                tasks.add(newTask);
+                addTaskMessage(newTask, tasks.size());
+
+            // Event tasks
+            } else if (command.equals("event")) {
+                String[] eventParts = inputParts[1].split(" /from ", 2);
+                String description = eventParts[0];
+                String[] timeParts = eventParts[1].split(" /to ", 2);
+                String from = timeParts[0];
+                String to = timeParts[1];
+                Task newTask = new Event(description, from, to);
+                tasks.add(newTask);
+                addTaskMessage(newTask, tasks.size());
+
             // Bye to exit
             } else if (command.equals("bye")) {
                 System.out.println(indent + "Bye. Hope to see you again soon!");
                 break;
 
-            // Add new task to new task list
             } else {
-                Task newTask = new Task(userInput);
-                tasks.add(newTask);
-                System.out.println(indent + "added: " + userInput);
+                System.out.println(indent + "Invalid command. Please try again!!!");
             }
         }
+    }
+    private static void addTaskMessage(Task task, int taskCount) {
+        String indent = "   ";
+        System.out.println(indent + "Got it. I've added this task:");
+        System.out.println(indent + "  " + task);
+        System.out.println(indent + "Now you have " + taskCount + " tasks in the list.");
     }
 }
